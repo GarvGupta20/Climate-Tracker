8bedcdf6b81b042b06b1ef96f04eced98bbb829f
Garv Gupta <garvg7904@gmail.com>
2023-01-30 23:12:06 +0530

    started the production using vite build toolchain with some extra confgurations

---
+++ Climate-Tracker/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  esbuild: {
+    jsxFactory: 'h',
+    jsxFragment: 'Fragment',
+    jsxInject:`import React from "react"
+     export default a => () { return <><div>Hi</div> </>}`
+  },
+})
+
